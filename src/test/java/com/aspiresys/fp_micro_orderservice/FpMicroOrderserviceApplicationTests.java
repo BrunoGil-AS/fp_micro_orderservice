ec8da2d820006745045d2cf851c77dc7dbb47535
BrunoGilRamirez <bruno.gilram@gmail.com>
2025-06-17 14:12:49 -0600

    first commit

---
+++ src/test/java/com/aspiresys/fp_micro_orderservice/FpMicroOrderserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aspiresys.fp_micro_orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FpMicroOrderserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
